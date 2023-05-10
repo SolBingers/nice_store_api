5ac2f98cdc5f68f42f928a88bbc859b893e53475
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-05-10 13:25:05 +0200

    changed paths

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Nice store api');
 });
 
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
