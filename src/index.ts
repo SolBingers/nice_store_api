b33e0fac6b8d767f6635c72d89a92b64ba998e1f
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-05-10 13:32:15 +0200

    installed cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { initDB } from './initDB';
 
 dotenv.config();
@@ -8,13 +9,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const sequelize = initDB();
+initDB();
+
+
+app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Nice store api');
 });
-
-app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
