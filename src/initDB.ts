18daf1194e5d205b4c5639b604ec84764b32c41e
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-05-11 14:46:11 +0200

    testing env urls

--- src/initDB.ts
+++ src/initDB.ts
@@ -3,8 +3,8 @@ import { Phone } from './models/Phone';
 import { Product } from './models/Product';
 import 'dotenv/config.js';
 
-// const { DB_URI = '' } = process.env;
-const DB_URI = 'postgres://kuznetsova-anastasiia:k5PBypHRAz9j@ep-summer-grass-565163.us-east-2.aws.neon.tech/neondb';
+const { DB_URI = '' } = process.env;
+// const DB_URI = 'postgres://kuznetsova-anastasiia:k5PBypHRAz9j@ep-summer-grass-565163.us-east-2.aws.neon.tech/neondb';
 
 export const initDB = async () => {
   const sequelize = new Sequelize(DB_URI, {
